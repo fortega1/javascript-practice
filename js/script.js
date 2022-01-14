e4e3196c248a6f72edd81a698d8965b93751b1cb
Felipe Ortega <felipe.j.ortega2018@gmail.com>
2022-01-13 20:25:39 -0800

    Day-2 Daily Challenge

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log('Supercalifragilisticexpialidocious'.length);
+console.log(3); //Any number
+console.log(); //BigInt
+
+console.log(typeof 3);  //typeof Any number
+console.log(typeof 10000000000n);  //BigInt
