9222a3efa6f7a7028b6685e7c4c29aa605378855
Felipe Ortega <felipe.j.ortega2018@gmail.com>
2022-02-03 21:03:08 -0800

    Day-7 class material

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// const url = "https://cat-fact.herokuapp.com/facts/random";
+
+// fetch(url)
+//     .then((response, reject) => {
+//         return response.json();
+//     })
+
+//     .then((data) => {
+//         // console.log(data);
+//         alert(data.text);
+//     })
+
+
+// Async-Await
+
+async function randomFact() {
+    let response = await fetch ("https://cat-fact.herokuapp.com/facts/random")
+    let data = await response.json()
+    alert(data.text)
+}
+
+randomFact()
+
+
