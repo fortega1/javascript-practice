297e78a20f2285d62fdd6dbec94d795e5b27e0fe
Felipe Ortega <felipe.j.ortega2018@gmail.com>
2022-02-08 18:16:25 -0800

    Update api-takehome

--- js/script.js
+++ js/script.js
@@ -1,26 +1,8 @@
-
-
-// const url = "https://cat-fact.herokuapp.com/facts/random";
-
-// fetch(url)
-//     .then((response, reject) => {
-//         return response.json();
-//     })
-
-//     .then((data) => {
-//         // console.log(data);
-//         alert(data.text);
-//     })
-
-
-// Async-Await
-
-async function randomFact() {
-    let response = await fetch ("https://cat-fact.herokuapp.com/facts/random")
-    let data = await response.json()
-    alert(data.text)
+function getPokemon() {
+fetch('https://pokeapi.co/api/v2/pokemon')
+	.then(response => response.json())
+	.then(data => {
+		alert(JSON.stringify(data));
+	})
 }
-
-randomFact()
-
 
