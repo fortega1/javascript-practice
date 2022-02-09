1074dff6dcef44188fed2219a289af774fb347c6
Felipe Ortega <felipe.j.ortega2018@gmail.com>
2022-02-08 19:26:41 -0800

    Update day-7

--- js/script.js
+++ js/script.js
@@ -20,3 +20,29 @@ async function randomFact() {
 }
 
 randomFact()
+
+// For specific ranges
+let count = 0;
+for(let i = 0; i < 20; i++){
+    console.log(i);
+    count += i;
+   }
+   
+   //For objects. Iterates through the object keys
+   // More on objects later
+   for(const key in obj) {
+       console.log(key)
+       console.log(obj[key])
+   }
+   
+   //For arrays
+   for(const item of arr) {
+       console.log(item)
+   }
+   // Not really a for loop but iterates through an array like for of
+   months.map(month => `${month} 2021`) // Returns array of altered values if any
+
+   //Interview Question: How to reverse an array
+   for (let index = array.length -1; index >=0; index--){
+       console.log(array[index]);
+   }
